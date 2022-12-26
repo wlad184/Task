e176a4e7c16ae2a491f81eb203218fbd8c365927
wlad184 <evel184@yandex.ru>
2022-12-26 17:37:46 +0700

    Решение задачи

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-
+        int[] arr ={11,22,33,44,55,66,77};
+        int index = arr.length - 1;
+        for (int i = 0; i < (arr.length)/2; i++) {
+            int a = arr[i];
+            arr[i] = arr[index];
+            arr[index] = a;
+            index--;
+        }
+        for (int j : arr) {
+            System.out.println(j);
+        }
     }
 }
